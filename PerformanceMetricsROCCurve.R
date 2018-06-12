1798c6a7b63903599d6d696f0811441cead3f019
Mark Meschi <mdmeschi72@gmail.com>
2018-06-11 19:09:41 -0500

    Moved to folders

--- PerformanceMetricsROCCurve.R
+++
@@ -1,7 +0,0 @@
-## with package ROCR from CRAN ## 
-library(ROCR)
-data(ROCR.simple)
-
-pred <- prediction (ROCR.simple$predictions, ROCR.simple$labels)
-perf <- performance(pred, "tpr", "fpr")
-plot(perf, lty=4, col="red")
